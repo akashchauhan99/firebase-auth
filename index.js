937bc81c3644110f0075aa977df11a3a762165d1
akashchauhan99 <chauhanakash4599@gmail.com>
2021-06-01 11:19:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyD0HhXWnW_2bn7vc0hUJpvluD3P1wEAMeM",
+    authDomain: "twfflours-ccccf.firebaseapp.com",
+    databaseURL: "https://twfflours-ccccf-default-rtdb.firebaseio.com",
+    projectId: "twfflours-ccccf",
+    storageBucket: "twfflours-ccccf.appspot.com",
+    messagingSenderId: "335949387220",
+    appId: "1:335949387220:web:449a40ba24f86aec333241",
+    measurementId: "G-8P660JY85E"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyAq3M9E4mRtoIV83mAKRsGI80-5rUMPPzM",
-        authDomain: "twfflours-8df74.firebaseapp.com",
-        projectId: "twfflours-8df74",
-        storageBucket: "twfflours-8df74.appspot.com",
-        messagingSenderId: "1085168336808",
-        appId: "1:1085168336808:web:d0d8bf6f1940df8404279d",
-        measurementId: "G-GXGG008BJY"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    // firebase.analytics();
+const auth = firebase.auth();
 
-    const auth = firebase.auth();
+function signUp() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var birthplace = document.getElementById("birthplace").value;
 
-    function signUp() {
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
-
-        const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-        promise.catch(e => alert(e.message));
+    auth.createUserWithEmailAndPassword(email, password).then(function() {
         alert("Signed Up");
-    }
-
-    function signIn() {
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
-
-        const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-        promise.catch(e => alert(e.message));
-        // alert("Signed In" + email);
-
-        //Take User to a different or home page
-    }
-
-    function signOut() {
-        auth.signOut();
-        alert("SignOut");
-    }
-
-    auth.onAuthStateChanged(function(user) {
-        if (user) {
-            var email = user.email;
-            alert("Active User " + email);
-        } else {
-            alert("No Active User");
-        }
+        var id = auth.currentUser.uid;
+        firebase.database().ref("Users/" + id).set({
+            userName: name,
+            userAge: age,
+            userBirthplace: birthplace,
+        });
+    }).catch(function(error) {
+        var errorcode = error.code;
+        var errormsg = error.message;
     });
+}
+
+function signIn() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    const promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(e => alert(e.message));
+    // alert("Signed In" + email);
+
+    //Take User to a different or home page
+}
+
+function signOut() {
+    auth.signOut();
+    alert("SignOut");
+}
+
+auth.onAuthStateChanged(function(user) {
+    if (user) {
+        var email = user.email;
+        alert("Active User " + email);
+    } else {
+        alert("No Active User");
+    }
+});
